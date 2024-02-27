65bea1801cf3c33d80291f45ca734703a6fdf52f
Victor Kamei Kay <kameikay@gmail.com>
2024-02-27 16:32:48 -0300

    add weather api service

---
+++ internal/service/weatherApi.go
@@ -0,0 +1,62 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type WeatherAPIResponse struct {
+	Current struct {
+		TempC float64 `json:"temp_c"`
+	} `json:"current"`
+}
+
+type WeatherApiServiceInterface interface {
+	GetWeatherData(ctx context.Context, location string) (*WeatherAPIResponse, error)
+}
+
+type WeatherApiService struct {
+	client *http.Client
+}
+
+func NewWeatherApiService() *WeatherApiService {
+	return &WeatherApiService{
+		client: &http.Client{},
+	}
+}
+
+func (s *WeatherApiService) GetWeatherData(ctx context.Context, location string) (*WeatherAPIResponse, error) {
+	WEATHER_API_KEY := viper.GetString("WEATHER_API_KEY")
+	location = strings.ReplaceAll(location, " ", "%20")
+	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no", WEATHER_API_KEY, location)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := s.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, errors.New("cannot find weather data")
+	}
+
+	var weatherAPIResponse WeatherAPIResponse
+	err = json.NewDecoder(res.Body).Decode(&weatherAPIResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &weatherAPIResponse, nil
+}
